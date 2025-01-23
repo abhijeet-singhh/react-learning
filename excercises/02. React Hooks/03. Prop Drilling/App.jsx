95cb6269254aa2a805e054baa703ac978f27b785
Abhijeet Singh <abhijeetsingh0104@gmail.com>
2025-01-23 18:14:02 +0530

    Add '03. Prop Drilling' topic to React Hooks section

---
+++ excercises/02. React Hooks/03. Prop Drilling/App.jsx
@@ -0,0 +1,14 @@
+import ComponentA from "./Components/ComponentA";
+
+function App() {
+
+    const name = 'Abhijeet'
+
+    return (
+        <div>
+            <ComponentA name={name} />
+        </div>
+    )
+}
+
+export default App;
