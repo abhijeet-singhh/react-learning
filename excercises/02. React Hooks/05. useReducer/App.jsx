c092c6110460a4475326981032284b17c7c478bf
Abhijeet Singh <abhijeetsingh0104@gmail.com>
2025-01-27 12:07:46 +0530

    Add '05. useReducer' excercise to React Hooks section

---
+++ excercises/02. React Hooks/05. useReducer/App.jsx
@@ -0,0 +1,10 @@
+import Counter from "./Components/Counter";
+
+function App() {
+
+    return <div className="min-h-screen flex justify-center items-center bg-stone-700">
+        <Counter />
+    </div>
+}
+
+export default App;
---
+++ excercises/02. React Hooks/05. useReducer/Counter.jsx
@@ -0,0 +1,41 @@
+import { useReducer, useState } from "react"
+import { counterReducer, initialState } from "../counterReducer"
+
+const Counter = () => {
+
+    const [state, dispatch] = useReducer(counterReducer, initialState)
+
+    const [inputValue, setInputValue] = useState(0)
+
+    const handleIncrement = () => dispatch({ type: 'increment' })
+    const handleDecrement = () => dispatch({ type: 'decrement' })
+    const handleReset = () => dispatch({ type: 'reset' })
+    const handleIncrementByAmount = () => {
+        dispatch({ type: 'incrementByAmount', payload: +inputValue })
+        setInputValue(0)
+    }
+    const handleDecrementByAmount = () => {
+        dispatch({ type: 'decrementByAmount', payload: +inputValue })
+        setInputValue(0)
+    }
+
+    return (
+        <div className="bg-white border-2 border-slate-400 p-4 rounded-md text-white">
+            <h2 className="font-bold text-2xl text-center text-slate-600">Counter using useReducer</h2>
+            <h2 className="bg-stone-200 rounded-md py-1 text-center text-xl font-semibold text-slate-600 mb-5 mt-6">Count: {state.count}</h2>
+            <div className="flex justify-around items-center">
+                <button className="w-20 bg-rose-700 px-2 py-1 rounded hover:bg-rose-800 active:scale-95 transition-all duration-200" onClick={handleIncrement}>+</button>
+                <button className="w-20 bg-rose-700 px-2 py-1 rounded hover:bg-rose-800 active:scale-95 transition-all duration-200" onClick={handleDecrement}>-</button>
+                <button className="w-20 bg-rose-700 px-2 py-1 rounded hover:bg-rose-800 active:scale-95 transition-all duration-200" onClick={handleReset}>Reset</button>
+            </div>
+
+            <div className="text-white">
+                <input className="w-full mt-3 text-slate-600 p-2 outline-none border-2 border-rose-400 focus:border-rose-600 rounded-md" type="number" value={inputValue} onChange={e => setInputValue(e.target.value)} />
+                <button className="w-full mt-3 bg-rose-700 px-2 py-1 rounded hover:bg-rose-800 active:scale-95 transition-all duration-200" onClick={handleIncrementByAmount}>Add</button>
+                <button className="w-full mt-1 bg-rose-700 px-2 py-1 rounded hover:bg-rose-800 active:scale-95 transition-all duration-200" onClick={handleDecrementByAmount}>Subtract</button>
+            </div>
+        </div>
+    )
+}
+
+export default Counter
