13b0be4db20ece3c022c6b73af61bbd7deef9398
Abhijeet Singh <abhijeetsingh0104@gmail.com>
2025-01-28 16:54:37 +0530

    [PROJECT] Add '01. Counter' project to Beginners Projects section

---
+++ projects/01. Beginners Projects/01. Counter/App.jsx
@@ -0,0 +1,11 @@
+import Counter from './components/Counter'
+
+const App = () => {
+    return (
+        <div className='min-h-screen bg-stone-700 flex justify-center items-center'>
+            <Counter />
+        </div>
+    )
+}
+
+export default App
