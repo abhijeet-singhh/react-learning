2c99e63d1cae87b6ddc4bc9a568a5ef1ed4da424
Abhijeet Singh <abhijeetsingh0104@gmail.com>
2025-04-25 15:38:49 +0530

    Add '08. Draggable Box' to Framer Motion Animations

---
+++ excercises/05. Framer Motion Animations/08. Draggable Box/App.tsx
@@ -0,0 +1,11 @@
+import DraggableBox from "./components/DraggableBox"
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-zinc-700 text-gray-50 flex justify-center items-center">
+      <DraggableBox />
+    </div>
+  )
+}
+
+export default App
